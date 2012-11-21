807403b07c54c99d53b194815acb58fcb9b6ca91
Thomas Darr <me@trdarr.com>
2012-11-21 00:53:33 -0600

    Change schema
    * Add tables for vocabulary features.
    * Collate everything as  utf8_unicode_ci.
    * Drop unused column `user`.`data`.
    * Remove default tables `attribute`, `item`, and `value`.

--- schema.sql
+++ schema.sql
@@ -16,27 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `attribute`
---
-
-DROP TABLE IF EXISTS `attribute`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `attribute` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `ascii_id` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `created_by` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `values_item_type` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `values_json` text COLLATE utf8_unicode_ci,
-  `input_type` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `applies_to_type` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `grammatical_feature_categories`
 --
 
@@ -47,9 +26,9 @@ CREATE TABLE `grammatical_feature_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +48,7 @@ CREATE TABLE `grammatical_feature_category_heirarchies` (
   KEY `child_id` (`child_id`),
   CONSTRAINT `grammatical_feature_category_heirarchies_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `grammatical_feature_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `grammatical_feature_category_heirarchies_ibfk_2` FOREIGN KEY (`child_id`) REFERENCES `grammatical_feature_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -84,12 +63,12 @@ CREATE TABLE `grammatical_features` (
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `category_id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `note` text,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `note` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `grammatical_features_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `grammatical_feature_categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -101,48 +80,14 @@ DROP TABLE IF EXISTS `grammatical_features_to_languages`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `grammatical_features_to_languages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `grammatical_feature_id` int(11) NOT NULL,
+  `feature_id` int(11) NOT NULL,
   `language_id` int(11) NOT NULL,
-  `source` text NOT NULL,
+  `source` text COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `feature_id` (`grammatical_feature_id`),
-  CONSTRAINT `grammatical_features_to_languages_ibfk_1` FOREIGN KEY (`grammatical_feature_id`) REFERENCES `grammatical_features` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `item`
---
-
-DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `serial_number` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `title` varchar(500) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `type` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `body` text COLLATE utf8_unicode_ci,
-  `url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `file_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `thumbnail_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `view_url` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `file_path` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `thumbnail_path` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `view_path` varchar(400) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `filesize` int(11) DEFAULT NULL,
-  `file_ext` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `file_original_name` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `mime` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `width` int(11) DEFAULT NULL,
-  `height` int(11) DEFAULT NULL,
-  `lat` float DEFAULT NULL,
-  `lng` float DEFAULT NULL,
-  `created` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `created_by` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `updated` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `updated_by` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  KEY `feature_id` (`feature_id`),
+  KEY `language_id` (`language_id`),
+  CONSTRAINT `grammatical_features_to_languages_ibfk_2` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `grammatical_features_to_languages_ibfk_1` FOREIGN KEY (`feature_id`) REFERENCES `grammatical_features` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -157,9 +102,15 @@ CREATE TABLE `language_families` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(244) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `name` varchar(244) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `created_by` (`created_by`),
+  KEY `updated_by` (`updated_by`),
+  CONSTRAINT `language_families_ibfk_2` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`),
+  CONSTRAINT `language_families_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,12 +124,48 @@ CREATE TABLE `languages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `family_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `family_id` (`family_id`),
   CONSTRAINT `languages_ibfk_1` FOREIGN KEY (`family_id`) REFERENCES `language_families` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `parts_of_speech`
+--
+
+DROP TABLE IF EXISTS `parts_of_speech`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parts_of_speech` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `semantic_fields`
+--
+
+DROP TABLE IF EXISTS `semantic_fields`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `semantic_fields` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `created_by` (`created_by`),
+  KEY `updated_by` (`updated_by`),
+  CONSTRAINT `semantic_fields_ibfk_2` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`),
+  CONSTRAINT `semantic_fields_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -194,24 +181,52 @@ CREATE TABLE `user` (
   `name` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(200) COLLATE utf8_unicode_ci DEFAULT NULL,
   `is_admin` tinyint(1) DEFAULT NULL,
-  `data` text COLLATE utf8_unicode_ci,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `value`
+-- Table structure for table `vocabulary_features`
 --
 
-DROP TABLE IF EXISTS `value`;
+DROP TABLE IF EXISTS `vocabulary_features`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `value` (
+CREATE TABLE `vocabulary_features` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `attribute_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `text` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created_by` int(11) NOT NULL,
+  `updated_by` int(11) NOT NULL,
+  `english` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `field_id` int(11) NOT NULL,
+  `pos_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `created_by` (`created_by`),
+  KEY `updated_by` (`updated_by`),
+  CONSTRAINT `vocabulary_features_ibfk_2` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`),
+  CONSTRAINT `vocabulary_features_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `vocabulary_features_to_languages`
+--
+
+DROP TABLE IF EXISTS `vocabulary_features_to_languages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vocabulary_features_to_languages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `feature_id` int(11) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  `original_form` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `ipa_form` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `feature_id` (`feature_id`),
+  KEY `language_id` (`language_id`),
+  CONSTRAINT `vocabulary_features_to_languages_ibfk_2` FOREIGN KEY (`language_id`) REFERENCES `languages` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `vocabulary_features_to_languages_ibfk_1` FOREIGN KEY (`feature_id`) REFERENCES `vocabulary_features` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -224,4 +239,4 @@ CREATE TABLE `value` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-11-20  3:37:44
+-- Dump completed on 2012-11-21  0:53:24
