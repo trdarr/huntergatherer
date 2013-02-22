48d9a26a65d0f030240c44d5a10e96c6abf6c1b9
Thomas Darr <me@trdarr.com>
2013-02-22 14:08:54 -0600

    Finish importer for grammar

--- Importer.php
+++ Importer.php
@@ -4,7 +4,7 @@ class Importer {
   // The number of database queries by type, for debugging.
   public static $queries = array();
 
-  public $methods; // An array of the names of this object's methods.
+  public $methods; // An array of the names of this object's public methods.
   private $cache;  // An array for caching the results of SQL queries.
   private $pdo;    // The PDO object, required by the constructor.
 
@@ -17,6 +17,33 @@ class Importer {
     $this->cache = array_fill_keys($methods, array());
     $this->methods = $methods;
     $this->pdo = $pdo;
+  }
+
+  // Generate cache functions to save some typing.
+  // TODO, maybe: move this into an object if you don't like functional PHP.
+  // TODO, maybe: if $cache_key is null, make functions require keys.
+  private function cache_functions($function, $cache_key) {
+    // Dynamically select which cache to pull from.
+    $cache = &$this->cache[$function];
+
+    // Generate functions for working with that specific cache.
+    // All functions handle an arbitrary key or the provided cache_key.
+    return array(
+      // cache_get: retrieve the value for a key.
+      function ($key=null) use (&$cache, $cache_key) {
+        return $cache[$key !== null ? $key : $cache_key];
+      },
+
+      // cache_set: cache a key/value pair. $value defaults to true.
+      function ($value=true, $key=null) use (&$cache, $cache_key) {
+        $cache[$key !== null ? $key : $cache_key] = $value;
+      },
+
+      // is_cached: does this key exist in the cache?
+      function ($key=null) use (&$cache, $cache_key) {
+        return isset($cache[$key !== null ? $key : $cache_key]);
+      },
+    );
   }
 
   // Query the database given some SQL and the parameters.
@@ -52,10 +79,9 @@ class Importer {
 
   // Handle the case of a single category (vs. a category/subcategory pair).
   private function grammatical_feature_category_one($category) {
-    // Shortcuts to save some typing.
-    $cache = &$this->cache[__FUNCTION__];
-    $cache_it = function ($k, $v) use (&$cache) { return $cache[$k] = $v; };
-    $is_cached = function ($k) use (&$cache) { return isset($cache[$k]); };
+    $cache_key = $category;
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
 
     // Build the array of parameters.
     $params = array(':category' => $category);
@@ -67,47 +93,44 @@ class Importer {
     // (It might be worth spending some time genericizing this routine.)
 
     // Check to see if it exists in the database.
-    if (!$is_cached($category)) {
+    if (!$is_cached()) {
       $sql = 'SELECT id, name
         FROM grammatical_feature_categories
         WHERE name = :category';
       $results = $this->query($sql, $params);
-      if (count($results) < 2)
-        $cache_it($category, $results[0]['id']);
-      else throw new Exception("Too many of $category.");
+      foreach ($results as $result) $cache_set($result['id']);
     }
 
     // Make it exist in the database if it doesn't.
-    if (!$is_cached($category)) {
+    if (!$is_cached()) {
       $sql = 'INSERT INTO grammatical_feature_categories
         (created_at, name) VALUES (NOW(), :name)';
       $this->query($sql, $params);
-      $cache_it($category, $this->pdo->lastInsertId());
+      $cache_set($this->pdo->lastInsertId());
     }
 
-    return $cache[$category];
+    return $cache_get();
   }
 
   // Handle the case of a category/subcategory pair (vs. a single category).
   private function grammatical_feature_category_two($category, $subcategory) {
-    // Shortcuts to save some typing.
-    $cache = &$this->cache[__FUNCTION__];
-    $cache_it = function ($k, $v) use (&$cache) { return $cache[$k] = $v; };
-    $is_cached = function ($k) use (&$cache) { return isset($cache[$k]); };
+    $cache_key = null;
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
 
     // Build the array of parameters.
-    $params = array(':category' => $category, ':subcategory' => $subcategory);
+    $params = array(
+      ':category' => $category,
+      ':subcategory' => $subcategory
+    );
 
     // Check to see if it exists in the database.
     if (!$is_cached($category) or !$is_cached($subcategory)) {
-      $sql = 'SELECT id, name
-        FROM grammatical_feature_categories
+      $sql = 'SELECT id, name FROM grammatical_feature_categories
         WHERE name = :category OR name = :subcategory';
       $results = $this->query($sql, $params);
-      if (count($results) < 3)
-        foreach ($results as $result)
-          $cache_it($result['name'], $result['id']);
-      else throw new Exception("Too many of $category or $subcategory.");
+      foreach ($results as $result)
+        $cache_set($result['id'], $result['name']);
     }
 
     // Make it exist in the database if it doesn't.
@@ -116,54 +139,47 @@ class Importer {
     foreach (array_values($params) as $name)
       if (!$is_cached($name)) {
         $this->query($sql, array(':name' => $name));
-        $cache_it($name, $this->pdo->lastInsertId());
+        $cache_set($this->pdo->lastInsertId(), $name);
       }
 
-    return array($cache[$category], $cache[$subcategory]);
+    return array($cache_get($category), $cache_get($subcategory));
   }
 
   // In case of a category/subcategory pair, encode the heirarchy.
   private function grammatical_feature_category_heirarchy($ids) {
-    // Shortcuts to save some typing.
-    list($parent_id, $child_id) = $ids;
-    $cache = &$this->cache[__FUNCTION__];
-    $cache_it = function ($k, $v) use (&$cache) { return $cache[$k] = $v; };
-    $is_cached = function ($k) use (&$cache) { return isset($cache[$k]); };
-    $key = '(' . implode(', ', $ids) . ')';
+    $cache_key = '(' . implode(', ', $ids) . ')';
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
 
     // Build the array of parameters.
+    list($parent_id, $child_id) = $ids;
     $params = array_combine(array(':parent_id', ':child_id'), $ids);
 
     // Check to see if it exists in the database.
-    if (!$is_cached($key)) {
+    if (!$is_cached()) {
       $sql = 'SELECT * FROM grammatical_feature_category_heirarchies
         WHERE parent_id = :parent_id AND child_id = :child_id';
       $results = $this->query($sql, $params);
-      if (count($results) < 2)
-        foreach ($results as $result)
-          $cache_it($key, $result['id']);
-      else throw new Exception("Too many of $key.");
+      foreach ($results as $result) $cache_set($result['id']);
     }
 
     // Make it exist in the database if it doesn't.
-    if (!$is_cached($key)) {
+    if (!$is_cached()) {
       $sql = 'INSERT INTO grammatical_feature_category_heirarchies
         (parent_id, child_id) VALUES (:parent_id, :child_id)';
       $this->query($sql, $params);
-      $cache_it($key, $this->pdo->lastInsertId());
+      $cache_set($this->pdo->lastInsertId());
     }
 
     return $child_id;
   }
 
-  public function grammatical_feature($feature) {
-    // Shortcuts to save some typing.
-    $cache = &$this->cache[__FUNCTION__];
-    $cache_it = function ($k, $v) use (&$cache) { return $cache[$k] = $v; };
-    $is_cached = function ($k) use (&$cache) { return isset($cache[$k]); };
+  private function grammatical_feature($feature) {
+    $cache_key = $name = $feature['grammaticalfeature'];
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
 
     // Build the array of parameters.
-    $name = $feature['grammaticalfeature'];
     $params = array(
       ':category_id' => $this->grammatical_feature_category($feature),
       ':name' => $name,
@@ -171,27 +187,84 @@ class Importer {
     );
 
     // Check to see if it exists in the database.
-    if (!$is_cached($name)) {
-      $sql = 'SELECT id, name
-        FROM grammatical_features
-        WHERE name = :name';
+    if (!$is_cached()) {
+      $sql = 'SELECT id, name FROM grammatical_features WHERE name = :name';
       $results = $this->query($sql, array(':name' => $name));
-      if (count($results) < 2)
-        foreach ($results as $result)
-          $cache_it($name, $result['id']);
-      else throw new Exception("Too many of $name.");
+      foreach ($results as $result) $cache_set($result['id']);
     }
 
     // Make it exist in the database if it doesn't.
-    if (!$is_cached($name)) {
+    if (!$is_cached()) {
       $sql = 'INSERT INTO grammatical_features
         (created_at, category_id, name, note)
         VALUES (NOW(), :category_id, :name, :notes)';
       $this->query($sql, $params);
-      $cache_it($name, $this->pdo->lastInsertId());
+      $cache_set($this->pdo->lastInsertId());
     }
 
-    return $cache[$name];
+    return $cache_get();
+  }
+
+  private function language($feature) {
+    $cache_key = $language = $feature['Language'];
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
+
+    // Build the array of parameters.
+    $params = array(':name' => $language);
+
+    // Check to see if it exists in the database.
+    if (!$is_cached()) {
+      $sql = 'SELECT id, name FROM languages WHERE name = :name';
+      $results = $this->query($sql, $params);
+      foreach ($results as $result) $cache_set($result['id']);
+    }
+
+    // We can't make it exist in the database if it doesn't already because
+    // there's not enough language data in the grammatical feature data files.
+    // The solution is to import language data first.
+
+    // Return null in that case.
+    return $is_cached() ? $cache_get() : null;
+  }
+
+  public function grammatical_feature_to_language($feature) {
+    $feature_id = $this->grammatical_feature($feature);
+    $language_id = $this->language($feature);
+    $cache_key = '(' . implode(', ', array($feature_id, $language_id)) . ')';
+    list($cache_get, $cache_set, $is_cached) =
+      $this->cache_functions(__FUNCTION__, $cache_key);
+
+    // Abort if we don't have any language data.
+    if (null === $language_id) return;
+
+    // Build the array of parameters.
+    $params = array(
+      ':feature_id' => $feature_id,
+      ':language_id' => $language_id,
+      ':answer' => $feature['Answer'],
+      ':source' => $feature['source'],
+    );
+
+    // Check to see if it exists in the database.
+    if (!$is_cached()) {
+      $sql = 'SELECT id, feature_id, language_id
+        FROM grammatical_features_to_languages
+        WHERE feature_id = :feature_id AND language_id = :language_id';
+      $results = $this->query($sql, array_slice($params, 0, 2));
+      foreach ($results as $result) $cache_set($result['id']);
+    }
+
+    // Make it exist in the database if it doesn't.
+    if (!$is_cached()) {
+      $sql = 'INSERT INTO grammatical_features_to_languages
+        (feature_id, language_id, answer, source)
+        VALUES (:feature_id, :language_id, :answer, :source)';
+      $this->query($sql, $params);
+      $cache_set($this->pdo->lastInsertId());
+    }
+
+    return $cache_get();
   }
 }
 
